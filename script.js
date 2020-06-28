97c43bc11ef73ea7857607714fe0cf2d869c5948
Michela Guzzo <michela.guzzo@gmail.com>
2020-06-28 15:31:52 +0200

    new script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var num = document.getElementById("result").innerText = 0;
+
 function getHistory(){
     return document.getElementById("history").innerText;
 }
@@ -10,7 +12,7 @@ function getOutput(){
 }
 function printOutput(num){
     if(num==""){
-        document.getElementById("result").innerText;
+        document.getElementById("result").innerText = num;
     }
     else{
     document.getElementById("result").innerText=formatNumber(num);
@@ -22,19 +24,59 @@ function formatNumber(num){
     return value;
 }
 
-function returnEmpty(){
-    return Number(num.remplace(/,/g,''));
-}
+var opequal = document.querySelector("#equal");
+var operatorperc = document.querySelector("#percentage").innerText;
+var buttbackspace = document.querySelector("#backspace");
+var buttclear = document.querySelector("#clear");
 
-var operator = document.getElementById("operator");
-for(var i = 0; i < operator.length; i++){
-    operator[i].addEventListener('click',function(){
 
+var temp = 0;
+var numbers = document.querySelectorAll('.number');
+for(let i = 0; i < numbers.length; i++){
+    numbers[i].addEventListener('click', function (){
+        if(temp.toString().includes('=')){
+            num = numbers[i].innerText;
+            temp = '0';
+            printHistory(' '); //reset the history after an operation
+        }
+        else if(num == '0'){
+            num = numbers[i].innerText;
+        }
+        else{
+            num = num + numbers[i].innerText;
+        }
+        printOutput(num);
     })
 }
-var number = document.getElementById("number");
-for(var i = 0; i < operator.length; i++){
-    number[i].addEventListener('click',function(){
-        
+
+
+var operators = document.querySelectorAll('.operator');
+for(let i = 0; i < operators.length; i++){
+    operators[i].addEventListener('click', function(){
+        if(temp == '0'){
+            temp = num + ' ' + operators[i].innerText;
+        }
+        else{
+            temp = temp + ' ' + num + ' ' + operators[i].innerText;
+        }
+        printHistory(temp);
+        num = 0;
+        printOutput(num);
     })
 }
+
+var result;
+opequal.addEventListener('click', function (){
+    temp = temp + ' ' + num;
+    result = eval(temp);
+    temp = temp + ' =';
+    printHistory(temp);
+    printOutput(result);
+    num = result;
+})
+
+buttbackspace.addEventListener('click', function(){
+    num = num.toString().splice((num.length - 1), 1);
+    printOutput(num);
+})
+
